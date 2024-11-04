f4f57392c06999fd7f3740c3abfe83333abc4ada
Bianca Soares <119140865+biancasuarz@users.noreply.github.com>
2024-11-04 11:05:55 -0300

    Adicionando Iterator

--- src/testes_aulas/Fila.java
+++ src/testes_aulas/Fila.java
@@ -12,12 +12,18 @@ public class Fila {
         for (int i = 0; i <= 10; i++) {
             fila.add(i);
         }
-        System.out.println("Fila: " + fila);
-        System.out.println("Removendo elemento da fila: " + fila.remove(2));
-        System.out.println(fila);
-        System.out.println("Adicionando elemento na fila: " + fila.add(2));
-        System.out.println(fila);
-        System.out.println("Exibir primeiro elemento da fila: " + fila.peek());
-        System.out.println("Exibir tamanho da fila: " + fila.size());
+        System.out.println("\nFila: " + fila);
+        System.out.println("\nRemovendo elemento da fila: " + fila.remove(2));System.out.println(fila);
+        System.out.println("\nAdicionando elemento na fila: " + fila.add(2));
+        System.out.println("\nExibir primeiro elemento da fila: " + fila.peek());
+        System.out.println("\nExibir tamanho da fila: " + fila.size());
+        System.out.println();
+
+        Iterator<Integer> iterator = fila.iterator();
+        while (iterator.hasNext()) {
+            System.out.println("Próximo da fila é: " + iterator.next());
+        }
+        fila.clear();
+        System.out.println("\nA fila está vazia: " + fila.isEmpty());
     }
 }
